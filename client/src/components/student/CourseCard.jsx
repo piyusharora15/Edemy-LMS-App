b5d8d8892c73c7ae824bea576073799db533e83b
Piyush Arora <piyusharora1504@gmail.com>
2025-10-11 14:59:14 +0530

    Added optional chaining

--- client/src/components/student/CourseCard.jsx
+++ client/src/components/student/CourseCard.jsx
@@ -11,8 +11,8 @@ const CourseCard = ({ course }) => {
         <Link onClick={() => scrollTo(0, 0)} to={'/course/' + course._id} className="border border-gray-500/30 pb-6 overflow-hidden rounded-lg">
             <img className="w-full" src={course.courseThumbnail} alt='' />
             <div className="p-3 text-left">
-                <h3 className="text-base font-semibold">{course.courseTitle}</h3>
-                <p className="text-gray-500">{course.educator.name}</p>
+                <h3 className="text-base font-semibold">{course?.courseTitle}</h3>
+                <p className="text-gray-500">{course?.educator.name}</p>
                 <div className="flex items-center space-x-2">
                     <p>{calculateRating(course)}</p>
                     <div className="flex">
@@ -25,9 +25,9 @@ const CourseCard = ({ course }) => {
                             />
                         ))}
                     </div>
-                    <p className="text-gray-500">({course.courseRatings.length})</p>
+                    <p className="text-gray-500">({course?.courseRatings.length})</p>
                 </div>
-                <p className="text-base font-semibold text-gray-800">{currency}{(course.coursePrice - course.discount * course.coursePrice / 100).toFixed(2)}</p>
+                <p className="text-base font-semibold text-gray-800">{currency}{(course?.coursePrice - course?.discount * course?.coursePrice / 100).toFixed(2)}</p>
             </div>
         </Link>
     )
